57ff93e1d2ef900605f63283372b4d01d29d8800
WonaKak10 <wonakak@gmail.com>
2018-01-14 22:47:39 +0300

    predicate full

--- src/main/java/lambdasinaction/chap3/Foo.java
+++ src/main/java/lambdasinaction/chap3/Foo.java
@@ -1,5 +1,8 @@
 package lambdasinaction.chap3;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
 public class Foo {
 //    public int  myMethod(int param) { return 1; }
     public char myMethod(int param) { return 'a';}
@@ -8,5 +11,32 @@ public class Foo {
         int portNumber = 1337;
         Runnable r = () -> System.out.println(portNumber);
 //        portNumber = 2222;
+    
+//        Predicate<Object> pred = isEqual(null);
+        Predicate<Object> pred = isEqual("123");
+        System.out.println(pred.test("123"));
+    }
+    
+    static <T> Predicate<T> isEqual(Object targetRef) {
+//        return (null == targetRef)
+//            ? Objects::isNull
+//            : targetRef::equals;
+    
+        if (targetRef == null) {
+            return (t) -> Objects.isNull(t);
+        } else {
+            return (t) -> targetRef.equals(t);
+        }
+        
+        /*return new Predicate<T>() {
+            @Override
+            public boolean test(T t) {
+                if (targetRef == null) {
+                    return Objects.isNull(t);
+                } else {
+                    return targetRef.equals(t);
+                }
+            }
+        };*/
     }
 }
